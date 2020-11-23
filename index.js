5c3288f621e4ae551083c92fda64e47fac97368b
morgancollado <Morgan.collado@gmail.com>
2020-11-23 15:01:22 -0600

    commit tree 38bad5282b55d96e74503a7d68474aa0accbc5ea

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, search) {
+
+    return array.filter( d => d.toLowerCase() === search.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(d => d.slice(0,string.length).toLowerCase() === string.toLowerCase())
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(d => d.name === string)
+}
